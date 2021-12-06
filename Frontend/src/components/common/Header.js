bdaff0fea8defae635856d894df452a9615a3abe
Big-Star <cwan961115@gmail.com>
2021-12-07 09:20:39 +1000

    Updated some pages's UI by Bigstar 2021-12-7

--- Frontend/src/components/common/Header.js
+++ Frontend/src/components/common/Header.js
@@ -34,16 +34,16 @@ export default function Header(props) {
                 </div>
 
                 <div className="header-right flex items-center">
-                    <div className="profile-dropdown-menu p-10 hidden">
-                        <div className="dropdown inline-block relative">
-                            <button className="profile-dropdown-btn capitalize main-font font-14 cursor-pointer m-0 hover-transition px-5  border-2 blue-border main-color-blue">
-                                <span className="mr-1">Dropdown</span>
-                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
+                    <div className="profile-dropdown-menu p-10 header-dropdown hidden">
+                        <div className="dropdown inline-block relative w-full">
+                            <button className="profile-dropdown-btn capitalize main-font font-14 cursor-pointer m-0  px-5  border-2 blue-border main-color-blue flex items-center hover-bg-blue hover:text-white">
+                                <span className="mr-1">Account</span>
+                                <svg className="fill-current h-4 w-4 ml-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
                             </button>
-                            <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
-                                <li className=""> <Link to="#">   <p className="profile-dropdown-item capitalize main-font font-14 cursor-pointer m-0 hover-transition px-5  border-2 blue-border main-color-blue ">aaa</p></Link></li>
-                                <li className=""> <Link to="#">   <p className="profile-dropdown-item capitalize main-font font-14 cursor-pointer m-0 hover-transition px-5  border-2 blue-border main-color-blue ">aaa</p></Link></li>
-                                <li className=""> <button type="button" className="profile-dropdown-item  capitalize main-font font-14 cursor-pointer m-0 hover-transition px-5  border-2 blue-border main-color-blue" onClick={handleLogout} >Log Out</button> </li>
+                            <ul className="dropdown-menu absolute hidden pt-1 border-2 blue-border rounded-lg">
+                                <li className="mt-3"> <Link to="#">   <p className="profile-dropdown-item border-b-2 rounded-none capitalize main-font font-14 hover-bg-blue hover:text-white cursor-pointer m-0  px-5  main-color-blue ">Account Setting</p></Link></li>
+                                <li className="mt-3"> <Link to="#">   <p className="profile-dropdown-item border-b-2 rounded-none capitalize main-font font-14 hover-bg-blue hover:text-white cursor-pointer m-0  px-5  main-color-blue ">Profile</p></Link></li>
+                                <li className="mt-3"> <button type="button" className="profile-dropdown-item border-b-2 rounded-none w-full text-left block capitalize main-font font-14 hover-bg-blue hover:text-white cursor-pointer m-0  px-5  main-color-blue" onClick={handleLogout} >Log Out</button> </li>
                             </ul>
                         </div>
                     </div>
