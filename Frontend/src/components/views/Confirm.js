79de5706133d2d0fcbd3a154fdb043f529a284eb
Big-Star <cwan961115@gmail.com>
2021-12-06 17:48:42 +1000

    Modified confirm page by Bigstar 2021-12-6

--- Frontend/src/components/views/Confirm.js
+++ Frontend/src/components/views/Confirm.js
@@ -31,6 +31,11 @@ export default class Confirm extends Component {
         <div className="main-container p-20">
           <div className="home-card mx-auto auth-form">
             <p className="text-center main-font font-30 main-color-blue mb-10 capitalize">Enter the verify code.</p>
+            <div className="confirm-text main-color main-font font-14 mb-10">
+              We want to make sure it's really you.
+              In order to further verify your identity.
+              Enter the verification code that was sent to your Email.
+            </div>
             <input
               type="text"
               className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded mb-10"
@@ -40,49 +45,8 @@ export default class Confirm extends Component {
               onChange={this.onChange}
               // value={this.state.verifyCode}
               autoComplete="off" />
-
-
-            {/* <form id="login-form" className="form" onSubmit={this.onSubmit}>
-              <GoogleLogin
-                clientId={GOOGLE_LOGIN_CLIENT_ID}
-                buttonText="Google Singup"
-                onSuccess={this.responseGoogle}
-                onFailure={this.responseGoogle}
-                className="google-button"
-                cookiePolicy={'single_host_origin'}
-              />
-              <input
-                type="email"
-                className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded mb-10"
-                name="email"
-                id="username"
-                autoComplete="none"
-                placeholder="Email"
-                value={this.state.email}
-                onChange={this.onChange}
-                autoComplete="off" />
-
-              <input
-                type="password"
-                className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded mb-10"
-                name="password"
-                autoComplete="none"
-                value={this.state.password}
-                onChange={this.onChange}
-                placeholder="Password"
-                autoComplete="off" />
-
-              <input
-                type="submit"
-                className="w-full text-center py-3 rounded button-bg text-white hover-transition font-14 main-font focus:outline-none m"
-                value="Login"
-              />
-
-              <div className="main-font main-color font-14 my-8 capitalize">
-                Do you want create an account?
-                <Link className="border-b border-gray-400 main-color-blue ml-5" to="/register">Sign Up Here</Link>
-              </div>
-            </form> */}
+          <button type="button" className="button-bg main-font text-white w-full block hover-transition blue-border rounded-lg py-3 my-10 font-16">Confirm</button>
+          <button type="button" className="bg-white main-font main-color-blue w-full block blue-border  hover-transition rounded-lg py-3 my-3 font-16">Resend Code</button>
           </div>
         </div>
         <Footer />
