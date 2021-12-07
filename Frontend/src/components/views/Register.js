1bf550138114aefe4dd6793092289d6107eba041
Victory Nicholas <vn649271@example.com>
2021-12-07 11:20:07 +1000

    - Implemented individual validation

--- Frontend/src/components/views/Register.js
+++ Frontend/src/components/views/Register.js
@@ -16,6 +16,9 @@ import { GoogleLogin } from 'react-google-login';
 
 const GOOGLE_LOGIN_CLIENT_ID = '533897933750-s85rovfjr2p6tg1pes1qdi89l8vo829g.apps.googleusercontent.com';
 
+const BATCHED_VALIDATION = 0; // Validation mode
+const INDIVIDUAL_VALIDATION = 1; // Validation mode
+
 var me;
 
 export default class Register extends Component {
@@ -105,119 +108,150 @@ export default class Register extends Component {
   }
 
   handleInputChange = event => {
+    console.log("Register.handleInputChange(): ", event.target.name, event.target.value)
     let input = this.state.input;
     input[event.target.name] = event.target.value;
-
     this.setState({
       input
     });
+    this.validate(event.target.name);
   }
 
   onPhone4EmailChange = val => {
     this.state.input.phone_for_email = val;
+    this.validate('phone_for_email');
   }
 
   onPhone4GmailChange = val => {
     this.state.input.phone_for_gmail = val;
   }
-  
-  validate = () => {
+
+  validate = (fieldName = null) => {
+    let validationMode = BATCHED_VALIDATION;
+    if (fieldName !== null && fieldName !== "") {
+      validationMode = INDIVIDUAL_VALIDATION;
+    }
     let input = this.state.input;
     let errors = {
-      first_name: '',
-      last_name: '',
-      email: '',
-      phone_for_email: '',
-      password: '',
-      confirm_password: '',
+      first_name: this.state.errors.first_name,
+      last_name: this.state.errors.last_name,
+      email: this.state.errors.email,
+      phone_for_email: this.state.errors.phone_for_email,
+      password: this.state.errors.password,
+      confirm_password: this.state.errors.confirm_password,
     };
     this.setState({ errors: errors });
 
     let isValid = true;
 
-    if (!input["first_name"]) {
-      isValid = false;
-      errors["first_name"] = "Please enter your first name.";
-    }
-
-    if (typeof input["first_name"] !== "undefined") {
-      const re = /^\S*$/;
-      if (input["first_name"].length < 1 || !re.test(input["first_name"])) {
+    // Validate first name
+    while (1) {
+      if (validationMode === INDIVIDUAL_VALIDATION && fieldName !== 'first_name')
+        break;
+      console.log('validation of first_name');
+      errors["first_name"] = "";
+      if (!input["first_name"]) {
         isValid = false;
-        errors["first_name"] = "Please enter valid first name.";
+        errors["first_name"] = "Please enter your first name.";
       }
+      if (typeof input["first_name"] !== "undefined") {
+        const re = /^\S*$/;
+        if (input["first_name"].length < 1 || !re.test(input["first_name"])) {
+          isValid = false;
+          errors["first_name"] = "Please enter valid first name.";
+        }
+      }
+      break;
     }
-
-    if (!input["last_name"]) {
-      isValid = false;
-      errors["last_name"] = "Please enter your last name.";
-    }
-
-    if (typeof input["last_name"] !== "undefined") {
-      const re = /^\S*$/;
-      if (input["last_name"].length < 1 || !re.test(input["last_name"])) {
+    // Validate last name
+    while (1) {
+      if (validationMode === INDIVIDUAL_VALIDATION && fieldName !== 'last_name')
+        break;
+      errors["last_name"] = "";
+      console.log('validation of last_name');
+      if (!input["last_name"]) {
         isValid = false;
-        errors["last_name"] = "Please enter valid last name.";
+        errors["last_name"] = "Please enter your last name.";
       }
+      if (typeof input["last_name"] !== "undefined") {
+        const re = /^\S*$/;
+        if (input["last_name"].length < 1 || !re.test(input["last_name"])) {
+          isValid = false;
+          errors["last_name"] = "Please enter valid last name.";
+        }
+      }
+      break;
     }
-
-    if (!input["email"]) {
-      isValid = false;
-      errors["email"] = "Please enter your email Address.";
-    }
-
-    if (typeof input["email"] !== "undefined") {
-      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-      if (!pattern.test(input["email"])) {
+    // Validate Email
+    while (1) {
+      if (validationMode === INDIVIDUAL_VALIDATION && fieldName !== 'email')
+        break;
+      errors["email"] = "";
+      if (!input["email"]) {
         isValid = false;
-        errors["email"] = 'Please enter valid email address (like "v@gmail.com")';
+        errors["email"] = "Please enter your email Address.";
       }
+      if (typeof input["email"] !== "undefined") {
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        if (!pattern.test(input["email"])) {
+          isValid = false;
+          errors["email"] = 'Please enter valid email address (like "v@gmail.com")';
+        }
+      }
+      break;
     }
-
-    if (!input["password"]) {
-      isValid = false;
-      errors["password"] = "Please enter your password.";
-    }
-
-    if (!input["confirm_password"]) {
-      isValid = false;
-      errors["confirm_password"] = "Please enter your confirm password.";
-    }
-
-    if (typeof input["password"] !== "undefined") {
-      if (input["password"].length < 6) {
+    // Validate password
+    while (1) {
+      if (validationMode === INDIVIDUAL_VALIDATION && fieldName !== 'password')
+        break;
+      errors["password"] = "";
+      if (!input["password"]) {
         isValid = false;
-        errors["password"] = "Please add at least 6 charachter.";
+        errors["password"] = "Please enter your password.";
       }
+      if (typeof input["password"] !== "undefined") {
+        if (input["password"].length < 6) {
+          isValid = false;
+          errors["password"] = "Please add at least 6 charachter.";
+        }
+      }
+      break;
     }
-
-    if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
-      if (input["password"] !== input["confirm_password"]) {
+    // Validate confirm password
+    while (1) {
+      if (validationMode === INDIVIDUAL_VALIDATION && fieldName !== 'confirm_password')
+        break;
+      errors["confirm_password"] = "";
+      if (!input["confirm_password"]) {
         isValid = false;
-        errors["confirm_password"] = "Passwords don't match.";
+        errors["confirm_password"] = "Please enter your confirm password.";
       }
+
+      if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
+        if (input["password"] !== input["confirm_password"]) {
+          isValid = false;
+          errors["confirm_password"] = "Passwords don't match.";
+        }
+      }
+      break;
     }
-
-    if (!input["phone_for_email"]) {
-      isValid = false;
-      errors["phone_for_email"] = 'Please enter phone number';
+    // Validate phone number format
+    while (1) {
+      if (validationMode === INDIVIDUAL_VALIDATION && fieldName !== 'phone_for_email')
+        break;
+      errors["phone_for_email"] = "";
+      if (!input["phone_for_email"]) {
+        isValid = false;
+        errors["phone_for_email"] = 'Please enter phone number';
+      }
+      break;
     }
-    // pattern = new RegExp(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/);
-    // if (!pattern.test(input["email"])) {
-    //   isValid = false;
-    //   errors["phone_for_email"] = 'Please enter valid phone number(10 digits)';
-    // }
-
-    // pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-    // if (!pattern.test(input["phone_for_email"])) {
-    //   isValid = false;
-    //   errors["phone_for_email"] = 'Please enter valid phone number';
-    // }
-
     this.setState({
       errors: errors
     });
-
+    if (validationMode == INDIVIDUAL_VALIDATION) {
+      isValid = false;
+    }
     return isValid;
   }
 
@@ -268,7 +302,7 @@ export default class Register extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.setState({ loading: true });    
+    this.setState({ loading: true });
     if (this.validate()) {
       this.recaptchaComponent.run(this.submitData);
     } else {
@@ -327,96 +361,96 @@ export default class Register extends Component {
               </div>
               <div className="w-1/2 signup-content border-l border-gray-300 pl-20 md:pl-10 pr-20">
                 {/* <form id="reg-form" className="form" onSubmit={this.onSubmit} autoComplete="off"> */}
-                  <div className="text-black">
-                    <div className="flex w-full">
-                      <div className="mb-10 w-full md:w-1/2">
-                        <input
-                          type="text"
-                          className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
-                          name="first_name"
-                          id="first_name"
-                          placeholder="First Name"
-                          value={this.state.first_name}
-                          onChange={this.handleInputChange} autoComplete="off" />
-                        <span className="help-block main-font text-red-400 font-16">{this.state.errors.first_name}</span>
-                      </div>
-                      <div className="mb-10 ml-2 w-full md:w-1/2">
-                        <input
-                          type="text"
-                          className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
-                          name="last_name"
-                          id="last_name"
-                          placeholder="Last Name"
-                          value={this.state.last_name}
-                          onChange={this.handleInputChange} autoComplete="off" />
-                        <span className="help-block main-font text-red-400 font-16">{this.state.errors.last_name}</span>
-                      </div>
-                    </div>
-                    <div className="mb-10 relative">
+                <div className="text-black">
+                  <div className="flex w-full">
+                    <div className="mb-10 w-full md:w-1/2">
                       <input
                         type="text"
                         className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
-                        name="email"
-                        id="email"
-                        placeholder="Email"
-                        value={this.state.email}
+                        name="first_name"
+                        id="first_name"
+                        placeholder="First Name"
+                        value={this.state.first_name}
                         onChange={this.handleInputChange} autoComplete="off" />
-                      <span className="help-block main-font text-red-400 font-14">{this.state.errors.email}</span>
+                      <span className="help-block main-font text-red-400 font-16">{this.state.errors.first_name}</span>
                     </div>
-                    <div className="mb-10">
-                      <PhoneInput
-                        placeholder="Enter phone number"
-                        name="phone_for_email"
-                        value={this.state.phone_for_email}
-                        onChange={this.onPhone4EmailChange}
-                        className="phone-for-email block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded" />
+                    <div className="mb-10 ml-2 w-full md:w-1/2">
+                      <input
+                        type="text"
+                        className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
+                        name="last_name"
+                        id="last_name"
+                        placeholder="Last Name"
+                        value={this.state.last_name}
+                        onChange={this.handleInputChange} autoComplete="off" />
+                      <span className="help-block main-font text-red-400 font-16">{this.state.errors.last_name}</span>
+                    </div>
+                  </div>
+                  <div className="mb-10 relative">
+                    <input
+                      type="text"
+                      className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                      value={this.state.email}
+                      onChange={this.handleInputChange} autoComplete="off" />
+                    <span className="help-block main-font text-red-400 font-14">{this.state.errors.email}</span>
+                  </div>
+                  <div className="mb-10">
+                    <PhoneInput
+                      placeholder="Enter phone number"
+                      name="phone_for_email"
+                      value={this.state.phone_for_email}
+                      onChange={this.onPhone4EmailChange}
+                      className="phone-for-email block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded" />
 
-                      <span className="help-block main-font text-red-400 font-14">{this.state.errors.phone_for_email}</span>
-                    </div>
-                    <div className="mb-10">
-                      <input
-                        type="password"
-                        className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleInputChange}
-                        placeholder="Password" autoComplete="off" />
-                      <span className="help-block main-font text-red-400 font-14">{this.state.errors.password}</span>
-                    </div>
-                    <div className="mb-10">
-                      <input
-                        type="password"
-                        className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
-                        name="confirm_password"
-                        id="confirm_password"
-                        value={this.state.confirm_password}
-                        onChange={this.handleInputChange}
-                        placeholder="Confirm Password" autoComplete="off" />
-                      <span className="help-block main-font text-red-400 font-14">{this.state.errors.confirm_password}</span>
-                    </div>
-                    {/* <input
+                    <span className="help-block main-font text-red-400 font-14">{this.state.errors.phone_for_email}</span>
+                  </div>
+                  <div className="mb-10">
+                    <input
+                      type="password"
+                      className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleInputChange}
+                      placeholder="Password" autoComplete="off" />
+                    <span className="help-block main-font text-red-400 font-14">{this.state.errors.password}</span>
+                  </div>
+                  <div className="mb-10">
+                    <input
+                      type="password"
+                      className="block border border-grey-light bg-gray-100  w-full p-5 font-16 main-font focus:outline-none rounded "
+                      name="confirm_password"
+                      id="confirm_password"
+                      value={this.state.confirm_password}
+                      onChange={this.handleInputChange}
+                      placeholder="Confirm Password" autoComplete="off" />
+                    <span className="help-block main-font text-red-400 font-14">{this.state.errors.confirm_password}</span>
+                  </div>
+                  {/* <input
                       type="submit"
                       className="w-full text-center py-5 rounded button-bg text-white hover-transition font-16 main-font focus:outline-none my-1"
                       value="Submit" autoComplete="off"
                     /> */}
-                    <button 
+                  <button
                     className="spinner-button w-full text-center py-5 rounded button-bg text-white hover-transition font-16 main-font focus:outline-none my-1"
-                    onClick={this.onSubmit} 
+                    onClick={this.onSubmit}
                     disabled={this.state.loading}>
-                      {this.state.loading && (
-                        <i
-                          className="fa od-spinner"
-                          style={{ marginRight: "15px" }}
-                        >( )</i>
-                      )}
-                      {this.state.loading && <span>Submitting</span>}
-                      {!this.state.loading && <span>Submit</span>}
-                    </button>
-                  </div>
-                  <div className="main-font main-color font-16 m-8 capitalize">
-                    Already have an account?
-                    <Link className="border-b border-gray-400 main-color-blue ml-5" to="/login">Login here</Link>
-                  </div>
+                    {this.state.loading && (
+                      <i
+                        className="fa od-spinner"
+                        style={{ marginRight: "15px" }}
+                      >( )</i>
+                    )}
+                    {this.state.loading && <span>Submitting</span>}
+                    {!this.state.loading && <span>Submit</span>}
+                  </button>
+                </div>
+                <div className="main-font main-color font-16 m-8 capitalize">
+                  Already have an account?
+                  <Link className="border-b border-gray-400 main-color-blue ml-5" to="/login">Login here</Link>
+                </div>
                 {/* </form> */}
               </div>
             </div>
