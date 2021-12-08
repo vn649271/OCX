6c68e5489de07c8a13d6184a788c992749644b62
Victory Nicholas <vn649271@gmail.com>
2021-12-08 21:59:03 +1000

    - Refined

--- Frontend/src/Contant.js
+++ Frontend/src/Contant.js
@@ -1,6 +1,7 @@
 const BACKEND_BASE_URL = "http://localhost:5000";
 // const BACKEND_BASE_URL = "https://openchaindexbackend.dt.r.appspot.com";
 const GOOGLE_LOGIN_CLIENT_ID = '533897933750-s85rovfjr2p6tg1pes1qdi89l8vo829g.apps.googleusercontent.com';
+const RECAPTCHA_SITE_KEY = "6LdoC28dAAAAACQ6Wbl7YPpOZVGHr9H-YQBKUkAA";
 const BATCHED_VALIDATION = 0; // Validation mode
 const INDIVIDUAL_VALIDATION = 1; // Validation mode
 const MAX_TIMEOUT = 30000;
@@ -11,6 +12,7 @@ const NOTIFY_INFORMATION = 1;
 module.exports = {
     BACKEND_BASE_URL, 
     GOOGLE_LOGIN_CLIENT_ID,
+    RECAPTCHA_SITE_KEY,
     BATCHED_VALIDATION,
     INDIVIDUAL_VALIDATION,
     MAX_TIMEOUT,
