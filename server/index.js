2c4e92136c5c78402f332dccb94927a12becb223
Victory Nicholas <vn649271@example.com>
2021-12-08 21:26:00 +1000

    - Remove unnecessary modules(because of failing to deploy) - Fixed Google-Login error

--- server/index.js
+++ server/index.js
@@ -5,30 +5,11 @@ var app = express();
 var session = require('express-session');
 var flash = require('express-flash');
 const cookieSession = require('cookie-session');
-const passport = require('passport');
-require('./utils/PassportPreset');
 require('dotenv').config();
 var randtoken = require('rand-token');
 
 var port = process.env.PORT;
 
-/**
- * Configure Gmail Authentication
- */
-var key1 = randtoken.generate(20);
-var key2 = randtoken.generate(20);
-app.use(cookieSession({
-    name: 'google-auth-session',
-    keys: ['key1', 'key2']
-}))
-
-app.use(session({ 
-    secret: '123458cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 }
-}))
-app.use(flash());
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -46,12 +27,6 @@ app.use("/users", Users);
  */
 var recaptchaRouter = require('./routes/Recaptcha');
 app.use('/recaptcha', recaptchaRouter);
-
-/**
- * Router for Gmail-Authentication
- */
- var gmailAuthRouter = require('./routes/GmailAuth');
-app.use('/google', gmailAuthRouter);
 
 app.listen(port, () => {
   console.log("server running at ", port);
