69699567645378865fe2b6bbfed527e8eb494962
Victory Nicholas <vn649271@gmail.com>
2021-12-11 17:33:39 +1000

    - Updated Backend

--- server/README.md
+++ server/README.md
@@ -1,25 +1,55 @@
-- Dependencies
-express
-express-flash
-express-session
-dotenv
-http-errors
-cookie-parser
-morgan
-passport
-cookie-session
-mysql
-nodemailer
-rand-token
-request
-passport-google-oauth2
+# React 😎 + Tailwind 💨 Project Starter Template 
+Use until create-react-app doesn't support PostCSS 8.
 
-- Server Port requirement
+## How to use?
+$git clone https://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@github.com/openchain123/OpenchainTestVersion.git`
+$yarn install
+    If you have not yarn installed, please run 'npm install yarn' before run 'yarn install'
+$npm run build
+$npm start
+
+## Why did I make this?
+I was using Tailwind with react a lot and felt the whole process of setting up tailwind every time quite the fuss.  
+
+#### Why the fuss?
+Create React App doesn't support PostCSS 8 yet so you need to install the Tailwind CSS v2.0 PostCSS 7. Also since Create React App doesn't let you override the PostCSS configuration natively, CRACO is used.
+
+#### PS: Star the repo if it helped you ⭐
+
+#### Run concurrently ⭐
+    "start": "concurrently --kill-others-on-fail \"nodemon server.js\" \"serve -s build -l 8080\"",
+    "dev": "concurrently --kill-others-on-fail \"npm run server-dev\" \"craco start\"",
+    "build": "craco build",
+    "test": "craco test",
+    "eject": "react-scripts eject"    
+
+#### Backend
+## Dependencies
+    express
+    express-flash
+    express-session
+    dotenv
+    http-errors
+    cookie-parser
+    morgan
+    passport
+    cookie-session
+    mysql
+    nodemailer
+    rand-token
+    request
+    passport-google-oauth2
+
+## Server Port requirement
 Port must be 5000. This value was registered with OAuth2.
 
-- Before run start backend
+## Before run start backend
 You must set project client id for Google Cloud Platform.
     https://cloud.google.com/docs/authentication/getting-started#windows
 For command prompt:
     set GOOGLE_APPLICATION_CREDENTIALS=KEY_PATH
 Replace KEY_PATH with the path of the JSON file that contains your service account key.
+
+
+
+
--- server/app/models/Firestore/Users.js
+++ server/app/models/Firestore/Users.js
@@ -3,8 +3,15 @@ const { Firestore } = require('@google-cloud/firestore');
 const db = new Firestore();
 const cllctn = 'users';
 
+/**
+ * Declaration of model object for user information
+ */
 function User() {
 
+    /**
+     * Find user information document by the specified conditions
+     * @param {json} jsonWhere search condition to be used
+     */
     this.findOne = async function (jsonWhere) {
 
         const usersRef = db.collection(cllctn);
@@ -13,7 +20,7 @@ function User() {
         for (let field in jsonWhere.where) {
             const snapshot = await usersRef.where(field, '==', jsonWhere.where[field]).get();
             if (snapshot.empty) {
-                console.log('No matching documents.');
+                console.info('No matching user information.');
                 return null;
             }
             snapshot.forEach(doc => {
@@ -25,6 +32,10 @@ function User() {
         return ret;
     }
 
+    /**
+     * Create a new user information document
+     * @param {object} jsonUser parameter object presenting new user information
+     */
     this.create = async function (jsonUser) {
         // Add a new document in collection "users"
         let now = new Date();
@@ -38,6 +49,11 @@ function User() {
         return null;
     }
 
+    /**
+     * Set pin code in the specified user information docment
+     * @param {string} userId id for the user information document to set status
+     * @param {string} pinCode pin code to be set
+     */
     this.setPinCode = async function (userId, pinCode) {
         const userRef = db.collection(cllctn).doc(userId);
         let now = new Date();
@@ -45,13 +61,27 @@ function User() {
         return res;
     }
 
+    /**
+     * Set access token to the server in the specified user information docment
+     * @param {string} userId id for the user information document to set status
+     * @param {token} token token to be set
+     */
     this.setToken = async function (userId, token) {
         const userRef = db.collection(cllctn).doc(userId);
         let now = new Date();
         const res = await userRef.update({ token: token, updated_at: now });
         return res;
     }
-
+    /**
+     * Set status in the specified user information docment
+     * @param {string} userId id for the user information document to set status
+     * @param {integer} status status to be set
+     */
+    this.setStatus = async function (userId, status) {
+        const userRef = db.collection(cllctn).doc(userId);
+        const res = await userRef.update({ status: status });
+        return res;
+    }
 }
 
 module.exports = User;
