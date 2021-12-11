f677178be710e1a51c08d0d955afebf87e32dc87
Victory Nicholas <vn649271@example.com>
2021-12-11 17:33:39 +1000

    - Updated Backend

--- server/app/controller/PhoneVerifyController.js
+++ server/app/controller/PhoneVerifyController.js
@@ -1,31 +1,47 @@
 var util = require('util');
 var exec = require('child_process').exec;
-
 const Phone = require("../models/Firestore/Phone");
 require('dotenv').config();
+
 
 const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 const PHONE_VERIFY_PENDING = 0;
 const PHONE_VERIFY_APPROVED = 1;
+const TWILIO_STATUS_CALLBACK_URL = process.env.TWILIO_STATUS_CALLBACK_URL;
 
 var me;
 
+/**
+ * Controller for phone verify
+ */
 class PhoneVerifyController {
     constructor() {
         me = this;
         this.phoneModel = new Phone();
-        this.urlForTwilioStatusCallback = 'http://localhost:5000/users/phoneVerifyStatus';
+        this.urlForTwilioStatusCallback = TWILIO_STATUS_CALLBACK_URL;
     }
 
+
+    /**
+     * 
+     * @param {object} req request object from the client 
+     * @param {object} res response object to the client
+     */
     getStatus = (req, resp) => {
-        console.log("PhoneVerifyController.getStatus():    ", req.body);
+        console.info("PhoneVerifyController.getStatus():    ", req.body);
     }
 
+    /**
+     * Validate the phone number
+     * @param {object} req request object from the client 
+     * @param {object} res response object to the client
+     */
     validatePhoneNumber(req, res) {
         var phoneNumber = req.body.phone.trim();
-        let command = 'curl -G https://lookups.twilio.com/v2/PhoneNumbers/' + phoneNumber +
-                    ' -u "' + process.env.ACCOUNT_SID + ":" + process.env.AUTH_TOKEN + '"';
+        let command = PHONE_VALIDATE_URL.replace('###', phoneNumber);
+        command = command.replace('@@@', process.env.ACCOUNT_SID);
+        command = command.replace('&&&', process.env.AUTH_TOKEN);
         exec(command, function (error, stdout, stderr) {
             const obj = JSON.parse(stdout);
             if (!obj.valid) {
@@ -37,27 +53,18 @@ class PhoneVerifyController {
                 } else if (errorText == "INVALID_BUT_POSSIBLE") {
                     errorText = "The phone number is invalid but possible";
                 }
-                res.json({ error: 1, message: errorText });
+                res.json({ error: -1, message: errorText });
                 return;
             }
             res.json({ error: 0 });
         });
     }
 
-    monitor = (from, to) => {
-        client.calls
-            .create({
-                method: 'GET',
-                statusCallback: this.urlForTwilioStatusCallback,
-                statusCallbackEvent: ['initiated', 'answered'],
-                statusCallbackMethod: 'POST',
-                url: 'http://demo.twilio.com/docs/voice.xml',
-                to: to,
-                from: from
-            })
-            .then(call => console.log(call.sid));
-    }
-
+    /**
+     * Send the request to get verification code to Twilio service
+     * @param {object} req request object from the client 
+     * @param {object} res response object to the client
+     */
     getCode = (req, res) => {
         var phoneNumber = "+" + req.query.phone.trim();
         this.phoneModel.findOne({
@@ -85,24 +92,29 @@ class PhoneVerifyController {
                                 };
                                 me.phoneModel.create(phoneInfo).then(newPhoneId => {
                                     if (newPhoneId === null) {
-                                        res.json({ error: 1, message: "Failed to register phone." });
+                                        res.json({ error: -1, message: "Failed to register phone." });
                                         return;
                                     }
                                     res.json({ error: 0, message: newPhoneId });
                                 });
                             } else {
-                                res.json({ error: 2, message: "Phone verifiy error: " + data.status });
+                                res.json({ error: -2, message: "Phone verifiy error: " + data.status });
                             }
                             return;
                         }
-                        res.json({ error: 3, message: "Failed to request code for phone verification. Please try to again later" });
+                        res.json({ error: -3, message: "Failed to request code for phone verification. Please try to again later" });
                     });
             } else {
-                res.json({ error: 10, message: "The phone is used for verification already" });
+                res.json({ error: -10, message: "The phone is used for verification already" });
             }
         })
     }
 
+    /**
+     * Send the request to verify provided code to Twilio service
+     * @param {object} req request object from the client 
+     * @param {object} res response object to the client
+     */
     verifyCode = (req, res) => {
         var phoneNumber = "+" + req.query.phone.trim();
         var verifyCode = req.query.code;
@@ -112,10 +124,10 @@ class PhoneVerifyController {
             }
         }).then(phoneInfo => {
             if (!phoneInfo) {
-                res.json({ error: 1, message: "Invalid phone number for verification" });
+                res.json({ error: -1, message: "Invalid phone number for verification" });
             } else {
                 if (phoneInfo.status != PHONE_VERIFY_PENDING) {
-                    res.json({ error: 2, message: "Invalid verify status" });
+                    res.json({ error: -2, message: "Invalid verify status" });
                     return;
                 }
                 var phoneId = phoneInfo.id;
@@ -133,10 +145,10 @@ class PhoneVerifyController {
                                 res.json({ error: 0, message: phoneId });
                                 return;
                             }
-                            res.json({ error: 3, message: "Phone verify status is invalid" });
+                            res.json({ error: -3, message: "Phone verify status is invalid" });
                             return;
                         }
-                        res.json({ error: 4, message: "Failed to verify phone" });
+                        res.json({ error: -4, message: "Failed to verify phone" });
                     });
             }
         });
