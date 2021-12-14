8149d46afaddc28145c57fab58de564975acdada
Big-Star <cwan961115@gmail.com>
2021-12-14 15:23:03 +1000

    added dashboard-layout by Bigstar 2021-12-14

--- Frontend/src/components/common/homepage/Trade.js
+++ Frontend/src/components/common/homepage/Trade.js
@@ -5,13 +5,14 @@ export default function Trade() {
         <div className="main-introduction py-20">
             <div className="small-container flex items-center responsive-col">
                 <div className="introduction-left w-1/2 responsive-w-90 responsive-flex-order2 ">
-                    <h1 className="intro-title main-color font-40 main-font capitalize">Trade anything. No registration, no hassle.</h1>
+                    <h1 className="intro-title main-color font-40 main-font capitalize">Third party digital ID verification system Making a peace of mind for everyone.</h1>
                     <p className="intro-description main-color font-24 main-font py-16 main-lineheight">
                         We are going to introduce our policies for KYC/AML third party verification system.
                     </p>
                     <div className="intro-bnt_group">
-                        <button className="hover-transition  text-white blue-border main-btn button-bg blue-border ">Trade Now</button>
-                        <button className="hover-transition main-color-blue main-btn">Learn</button>
+                        <a href='https://www/digitalid.com/developer' className='font-24 main-color-blue'>https://www/digitalid.com/developer</a>
+                        {/* <button className="hover-transition  text-white blue-border main-btn button-bg blue-border ">Trade Now</button>
+                        <button className="hover-transition main-color-blue main-btn">Learn</button> */}
                     </div>
                 </div>
                 <div className="introduction-right w-1/2 pl-40 responsive-p-30 responsive-flex-order1 ">
