4d6b117db2879ea817c4178c42c78e2bac3be433
Victory Nicholas <vn649271@example.com>
2021-12-19 01:34:52 +1000

    Updated deploy scripts for recreated GC projects

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -3,11 +3,11 @@ cd ~/OpenchainTestVersion
 git pull
 cd Frontend
 echo "********************************** docker build *****************************************************"
-docker build -t openchaindex-image-1 .
+docker build -t openchaindex-frontend-image-1 .
 echo "********************************** gcloud auth login *****************************************************"
 gcloud auth login
 gcloud auth configure-docker
-gcloud config set project openchaindex
+gcloud config set project openchaindex-frontend
 echo "********************************** gcloud build *****************************************************"
-gcloud builds submit --tag gcr.io/openchaindex/openchaindex-image-1
+gcloud builds submit --tag gcr.io/openchaindex-frontend/openchaindex-frontend-image-1
 echo "****************Built your docker image successfully. It's time to register with service **************"
