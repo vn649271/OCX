b54def2f4f16adb507e1c9eb8ede26739d9d4a01
Victory Nicholas <vn649271@gmail.com>
2021-12-19 01:34:52 +1000

    Updated deploy scripts for recreated GC projects

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -7,7 +7,7 @@ docker build -t openchaindex-backend-image-1 .
 echo "********************************** gcloud auth login *****************************************************"
 gcloud auth login
 gcloud auth configure-docker
-gcloud config set project openchaindexbackend
+gcloud config set project openchaindex-backend
 echo "********************************** gcloud build *****************************************************"
-gcloud builds submit --tag gcr.io/openchaindexbackend/openchaindex-backend-image-1
+gcloud builds submit --tag gcr.io/openchaindex-backend/openchaindex-backend-image-1
 echo "****************Built your docker image successfully. It's time to register with service **************"
