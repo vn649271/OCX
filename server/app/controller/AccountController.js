77861b4a8c16b97f818735da451d54b1b584d32f
Victory Nicholas <vn649271@example.com>
2021-12-30 17:33:23 +1000

    - Fixed sending

--- server/app/controller/AccountController.js
+++ server/app/controller/AccountController.js
@@ -1,66 +1,59 @@
 var util = require('util');
 require('dotenv').config();
-const { exec, spawn } = require('child_process');
 const jwt = require("jsonwebtoken");
 const User = require("../models/Firestore/Users");
-const Phone = require("../models/Firestore/Account");
-var fs = require("fs");
-const net = require('net');
 const Web3 = require('web3');
-var Tx = require('ethereumjs-tx').Transaction;
-const CommonUtils = require('../utils/CommonUtils');
+const net = require('net');
+var fs = require("fs");
+const { spawn } = require('child_process');
+const { json } = require('body-parser');
 
-let commonUtils = new CommonUtils();
-
-var userModel = new User();
-
-var self = null;
-
+const MY_ACCOUNT_PASSWORD = process.env.MY_ACCOUNT_PASSWORD || "Hdmq8e820.";
+const UNLOCK_ACCOUNT_INTERVAL = process.env.UNLOCK_ACCOUNT_INTERVAL || 15000; // 15s
 const ETHER_NETWORK = process.env.ETHER_NETWORK || "goerli";
+const ipcPath = process.env.HOME + "/.ethereum/" + ETHER_NETWORK + "/geth.ipc";
+
 
 var geth = spawn('geth', ['--goerli', '--syncmode', 'light']);
 geth.stdout.on('data', (data) => {
     console.log(`geth:stdout: ${data}`);
 });
 geth.stderr.on('data', (data) => {
-    console.log("ggggggggggggggggggggggggggggg: ", typeof data);
-    if (typeof data === 'object') {
-        let retStr = new Buffer.from(data).toString();
-        if (retStr.length > 4 && retStr.substring(0, 5) === 'INFO ') {
-          return; // If string is INFO message, ignore
-        }
-        if (self !== null) {
-            self.setGethError(retStr);
-        }
-    }
+    // console.log("ggggggggggggggggggggggggggggg: ", typeof data);
+    // if (typeof data === 'object') {
+    //     let retStr = new Buffer.from(data).toString();
+    //     if (retStr.length > 4 && retStr.substring(0, 5) === 'INFO ') {
+    //       return; // If string is INFO message, ignore
+    //     }
+    //     if (self !== null) {
+    //         self.setGethError(retStr);
+    //     }
+    // }
     console.log(`geth:stderr: ${data}`);
 });
 geth.on('close', (code) => {
     console.log(`geth: child process exited with code ${code}`);
 });
 
+var gethIpcTimer = null;
 var web3 = null;
-// const ipcPath = process.env.HOME + "/.ethereum/" + ETHER_NETWORK + "/geth.ipc";
-const ipcPath = "\\\\.\\pipe\\geth.ipc";
+var myAccount = null;
 
-var gethIpcTimer = setTimeout(
-    function() {
-        fs.access(ipcPath, (err) => {
-          if (!err) {
+function attachToGethIPC() {
+    fs.access(ipcPath, (err) => {
+        if (!err) {
+            console.log("Attached to Geth IPC successfully");
             web3 = new Web3(new Web3.providers.IpcProvider(ipcPath, net));
             if (!web3) {
                 return;
             }
             clearTimeout(gethIpcTimer);
             gethIpcTimer = null;
-            return;
-          }
-          console.log('&&&&&&&&&&&&&&&&&&& Geth does not exist yet');
-        });
-    },
-    5000
-);
+        }
+    });
+}
 
+gethIpcTimer = setTimeout(attachToGethIPC, 10000);
 
 
 /**
@@ -146,9 +139,17 @@ class AccountController {
         if (!ret || ret.error < 0) {
             return resp.json({ error: -2, data: "Invalid user token" });
         }
-        let account = userModel.getAccount(userToken);
-        web3.eth.getBalance(account).then(function(balance) {
-            resp.json({error: 0, data: balance});
+        web3.eth.personal.getAccounts().then(function(accounts) {
+            if (accounts.length < 1) {
+                console.log("No account");
+                return resp.json({ error: -3, data: "No account for you"});
+            }
+            myAccount = accounts[0];
+            web3.eth.getBalance(myAccount).then(function(balanceInWei) {
+                console.log(balanceInWei);
+                let balance = web3.utils.fromWei(balanceInWei, 'ether');
+                resp.json({ error: 0, data: balance });
+            });
         });
     }
 
@@ -177,63 +178,32 @@ class AccountController {
             return resp.json({ error: -4, data: "Invalid sending amount" });
         }
 
-        var sendingInfo = {
-            myAddress: ret.ether.address,
-            lastNonce: ret.ether.nonce - 0,
-            toaddress: req.body.toAddress,
-            amount: req.body.amount
-        }
+        var toAddress = req.body.toAddress;
+        var toAmount = req.body.amount;
 
-        web3.eth.getBlock("latest").then(function(latestBlock) {
-            if (latestBlock === undefined || latestBlock === null) {
-                return resp.json({ error: -11, data: "Failed to get latest block" });
+        web3.eth.personal.getAccounts().then(function(accounts) {
+            if (accounts.length < 1) {
+                console.log("No account");
+                return;
             }
-            var gasPrice = latestBlock.gasPrice;//or get with web3.eth.gasPrice
-            var gasLimit = latestBlock.gasLimit;
-            var nonce = web3.eth.getTransactionCount(sendingInfo.myAddress); //211;
-
-            var rawTransaction = {
-                "from": sendingInfo.myAddress,
-                "nonce": web3.utils.toHex(nonce),
-                "gasPrice": web3.utils.toHex(gasPrice/* * 0x1e9*/),
-                "gasLimit": web3.utils.toHex(gasLimit),
-                "to": sendingInfo.toAddress,
-                "value": web3.utils.toHex(sendingInfo.amount),
-                "chainId": 5 //remember to change this
-            };
-          
-            var tx = new Tx(rawTransaction, {'chain': self.chainName});
-
-            var privateKey = Buffer.from('9fe71fcea7a64c4f1e6e6c90d34d869367f13ad94392f3ed6117ff9969922979', 'hex');
-            tx.sign(privateKey);
-            
-            var serializedTx = tx.serialize();
-            
-            // console.log(serializedTx.toString('hex'));
-            // 0xf889808609184e72a00082271094000000000000000000000000000000000000000080a47f74657374320000000000000000000000000000000000000000000000000000006000571ca08a8bbf888cfa37bbf0bb965423625641fc956967b81d12e23709cead01446075a01ce999b56a8a88504be365442ea61239198e23d1fce7d00fcfc5cd3b44b7215f
-            
-            web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-            .on('transactionHash', function(hash){
-                // When retrieve transaction hash
-                console.log("The transaction retrieved a hash: ", hash);
-                return resp.json({ error: 0, data: hash });
-            })
-            .on('receipt', function(receipt){
-                console.log("The transaction retrieved receipt: ", receipt);
-                return resp.json({ error: 1, data: receipt });
-            })
-            .on('confirmation', function(confirmationNumber, receipt) { 
-                // When the transaction confirmed
-                console.log("The transaction confirmed: Confirmation number: ", confirmationNumber, receipt);
-                return resp.json({ error: 2, data: receipt });
-            })
-            .on('error', function(err) {
-                console.log("1111111111111111111111111111111111: ", err);
-                let errStr = self.gethError;
-                console.log("2222222222222222222222222222222222: ", errStr);
-                return resp.json({ error: -10, data: errStr });
-            }); // If a out of gas error, the second parameter is the receipt.
-            
+            console.log("Accounts: ", accounts);
+            myAccount = accounts[0];
+            web3.eth.personal.unlockAccount(myAccount, MY_ACCOUNT_PASSWORD, UNLOCK_ACCOUNT_INTERVAL)
+            .then(function(ret) {
+                console.log("Unlocking: ", ret);
+                if (!ret) {
+                    console.log("Failed to unlock the account");
+                    return;
+                }
+                let amountToSend = web3.utils.toWei(toAmount.toString(), "ether");
+                web3.eth.sendTransaction({
+                    "from": myAccount,
+                    "to": toAddress,
+                    "value": amountToSend
+                }).then(function(txHash) {
+                    return resp.json({error: 0, data: txHash});
+                });
+            });
         });
     }
 };
