3371ed6c16b4b6963af852eb7266c20f19f9e026
Victory Nicholas <vn649271@gmail.com>
2022-01-09 00:04:50 +1000

    - Fixed for deployment

--- Frontend/src/Contants.js
+++ Frontend/src/Contants.js
@@ -1,5 +1,5 @@
-const BACKEND_BASE_URL = "http://localhost:5000/api";
-// const BACKEND_BASE_URL = "https://openchaindex-backend-image-1-tog6a3zdpa-uc.a.run.app/api";
+// const BACKEND_BASE_URL = "http://localhost:5000/api";
+const BACKEND_BASE_URL = "https://openchaindex-backend-image-1-tog6a3zdpa-uc.a.run.app/api";
 const GOOGLE_LOGIN_CLIENT_ID = '760003596258-gien4jnstrtgq07u46b14ifa44cmtub0.apps.googleusercontent.com';
 const RECAPTCHA_SITE_KEY = "6LfcUmYdAAAAAIRMTrBktzC2ONu5PlgnOPKkzUtY";
 const BATCHED_VALIDATION = 0; // Validation mode
