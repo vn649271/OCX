b18afa1180279152e9dce249fa2a3c955d0503d6
Victory Nicholas <vn649271@gmail.com>
2022-01-13 20:15:27 +1000

    - Added components for Swap

---
+++ Frontend/src/components/common/SwapView.js
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+
+var self;
+
+class SwapView extends Component {
+
+    constructor(props) {
+        super(props);
+        self = this;
+
+        this.state = {
+        }
+    }
+
+    render() {
+        return(
+            <div className="swap-container">
+                Swap Container
+            </div>
+        );
+    }
+}
+
+export default SwapView;
