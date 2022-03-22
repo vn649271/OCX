c56bc6e74f319de596133aee3817946009596f87
Victory Nicholas <vn649271@gmail.com>
2022-03-23 03:30:43 +1000

    - Fixed action buttons track table: changed <DelayButton> to <div>

---
+++ Frontend/src/components/common/SpinButton.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+var me;
+
+class DelayButton extends Component {
+
+    constructor(props) {
+        super(props);
+        me = this;
+
+        this.captionInDelay = props.captionInDelay;
+        this.caption = props.caption;
+        this.maxDelayInterval = props.maxDelayInterval;
+        this.onClickButton = props.onClickButton;
+        this.onClickButtonParam = props.onClickButtonParam;
+
+        this.state = {
+            value: '',
+            in_delay: false,
+            delay_interval: this.maxDelayInterval
+        }
+
+        this.onClicked = this.onClicked.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+    }
+
+    stopTimer() {
+        this.setState({ in_delay: false });
+        this.setState({ delay_interval: this.maxDelayInterval });
+        clearTimeout(this.delayTimer);
+    }
+
+    onClicked = (ev) => {
+        if (this.state.in_delay && this.state.delay_interval > 0) {
+            return;
+        }
+        if (ev.target.nodeName == "SPAN") {
+            ev.target = ev.target.parentNode;
+        }
+
+        this.setState({ in_delay: true });
+
+        /* Start counting for loading data */
+        this.delayTimer = setTimeout(
+            function () {
+                let delayInterval = me.state.delay_interval;
+                delayInterval--;
+                if (delayInterval <= 0) {
+                    me.stopTimer();
+                } else {
+                    me.setState({ delay_interval: delayInterval });
+                }
+            },
+            1000
+        );
+        this.onClickButton(this.onClickButtonParam, ev, this);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.target !== this.props.target) {
+        }
+    }
+
+    render() {
+        return (
+            <div {...this.props}>{this.props.children}
+            </div>
+        );
+    }
+
+}
+
+export default DelayButton;
