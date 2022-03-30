9be574da88d44c5831420a2f7579586a4e227e20
Victory Nicholas <vn649271@gmail.com>
2022-03-30 15:59:36 +0900

    - Updated error messages in contract

--- server/contracts/PawnExchange.sol
+++ server/contracts/PawnExchange.sol
@@ -27,26 +27,26 @@ contract PawnExchange {
     }
 
     function setPnftAddress(address payable _pnftAddress) public {
-        require(creator == msg.sender, "OcxExchange.setPNFTAddress(): Caller for  must be creator");
-        require(_pnftAddress != address(0), "OcxExchange.setPNFTAddress(): Invalid parameter");
+        require(creator == msg.sender, "PawnExchange.setPNFTAddress(): Caller for  must be creator");
+        require(_pnftAddress != address(0), "PawnExchange.setPNFTAddress(): Invalid parameter");
         pnftAddress = _pnftAddress;
     }
 
     function setOcatAddress(address payable _ocatAddress) public {
-        require(creator == msg.sender, "OcxExchange.setOcatAddress(): Caller for  must be creator");
-        require(_ocatAddress != address(0), "OcxExchange.setOcatAddress(): Invalid parameter");
+        require(creator == msg.sender, "PawnExchange.setOcatAddress(): Caller for  must be creator");
+        require(_ocatAddress != address(0), "PawnExchange.setOcatAddress(): Invalid parameter");
         ocatAddress = _ocatAddress;
     }
 
     function exchangeToOcat(uint256 nftID) public {
-        require(msg.sender != address(this), "exchangeToOcat(): Error: Caller couldn't be same to this address");
-        require(msg.sender != address(0), "exchangeToOcat(): Invalid caller");
-        require(nftID > 0, "exchangeToOcat(): Invalid NFT ID");
+        require(msg.sender != address(this), "PawnExchange.exchangeToOcat(): Error: Caller couldn't be same to this address");
+        require(msg.sender != address(0), "PawnExchange.exchangeToOcat(): Invalid caller");
+        require(nftID > 0, "PawnExchange.exchangeToOcat(): Invalid NFT ID");
         uint256 ocatBalance = IERC20(ocatAddress).balanceOf(address(this));
         // Get price for the NFT
         (,,,,,,uint256 price,,) = PawnNFTs(payable(address(pnftAddress))).allPawnNFTs(nftID);
         // uint256 price = nftItem.price;
-        require(ocatBalance >= price, "Insufficient balance of OCAT in the contract");
+        require(ocatBalance >= price, "PawnExchange.exchangeToOcat(): Insufficient balance of OCAT in the contract");
         // safeTransferFrom: send NFT from caller to the address
         IERC721(pnftAddress).safeTransferFrom(msg.sender, address(this), nftID);
         // Pay OCAT for the NFT
@@ -56,14 +56,14 @@ contract PawnExchange {
     }
 
     function exchangeFromOcat(uint256 nftID) public {
-        require(msg.sender != address(this), "exchangeToOcat(): Error: Caller couldn't be same to this address");
-        require(msg.sender != address(0), "exchangeToOcat(): Invalid caller");
-        require(nftID > 0, "exchangeToOcat(): Invalid NFT ID");
+        require(msg.sender != address(this), "PawnExchange.exchangeFromOcat(): Error: Caller couldn't be same to this address");
+        require(msg.sender != address(0), "PawnExchange.exchangeFromOcat(): Invalid caller");
+        require(nftID > 0, "PawnExchange.exchangeFromOcat(): Invalid NFT ID");
         uint256 ocatBalance = IERC20(ocatAddress).balanceOf(msg.sender);
         // Get price for the NFT
         (,,,,,,uint256 price,,) = PawnNFTs(payable(address(pnftAddress))).allPawnNFTs(nftID);
         // uint256 price = nftItem.price;
-        require(ocatBalance >= price, "Insufficient balance of OCAT in the account");
+        require(ocatBalance >= price, "PawnExchange.exchangeFromOcat(): Insufficient balance of OCAT in the account");
         // safeTransferFrom: send NFT from caller to the address
         TransferHelper.safeTransferFrom(ocatAddress, msg.sender, address(this), price);
         // Pay OCAT for the NFT
