bff546aea523a9342e570faf718c6adf38379d0b
Victory Nicholas <vn649271@example.com>
2022-04-05 07:44:30 +1000

    + Added PriceOracle smart contract template from Chainlink

---
+++ server/contracts/PriceOracle.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract GetPriceFeed {
+
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    }
+
+    function getETHPriceInUSD() public view returns (int) {
+        (
+            , 
+            int price,
+            ,
+            ,
+            
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
