0e0596f8a979d367436068e08f6e884b51ff10d7
Victory Nicholas <vn649271@gmail.com>
2022-04-12 16:10:49 +1000

    - Implemented logic for ready checkbox in wallet intro page

--- Frontend/src/components/common/OcxButton.js
+++ Frontend/src/components/common/OcxButton.js
@@ -1,22 +1,27 @@
-import React, { Component, useStyles } from 'react';
-
-var me;
+import { useState } from 'react';
 
 export default function OcxButton(props) {
 
-    const { label, onClick = null } = props;
+    const { label, onClick = null, enabled = 1 } = props;
 
     const defaultClass = "main-font font-16 border border-grey-light p-3 w-max button-bg focus:outline-none rounded text-white hover-transition cursor-pointer";
+    const disabledClass = "main-font font-16 border border-grey-light p-3 w-max button-bg rounded text-white cursor-pointer opacity-50";
 
-    const [buttonClass, setButtonClass] = React.useState(defaultClass);
+    const [buttonClass, setButtonClass] = useState(defaultClass);
+    const [status, setStatus] = useState(enabled); // 1: enabled, 0: disabled
 
     const handleClick = () => {
+        if (!enabled) return;
     	if (onClick) onClick();
     };
 
+    const setEnable = (enable) => {
+        enabled = enable ? 1: 0;
+    }
+
     return (
         <div
-            className={buttonClass} 
+            className={enabled ? buttonClass : disabledClass } 
             onClick={handleClick}
         >{label}</div>
     );
