fcd093fececa031d329ab4e36f0562ea20f60552
Victory Nicholas <vn649271@example.com>
2022-04-15 04:19:07 +1000

    - Fixed spinning effect on Mint button in pawnshop track table

--- Frontend/src/components/common/SimpleTable.js
+++ Frontend/src/components/common/SimpleTable.js
@@ -1,22 +1,16 @@
 import { useState, useEffect } from "react";
 import OcxSpinner from './OcxSpinner';
 
-const TableTrs = (props) => {
-
-}
-
 const SimpleTable = props => {
 
     const {colDef = null, tableData = null} = props;
 
-    // const [_colDef, setColDef] = useState(colDef);
-    const [_data, setData] = useState(tableData);
-    const [trs, setTRs] = useState([]);
+    const [_colDef, setColDef] = useState(colDef);
+    const [_tableData, setData] = useState(tableData);
 
     useEffect(() => {
+        setColDef(colDef);
         setData(tableData);
-        var _trs = [];
-        return null;
     });
 
     return (
@@ -28,7 +22,7 @@ const SimpleTable = props => {
                             <thead className="bg-gray-50">
                                 <tr>
                                 {
-                                    colDef ? colDef.headers ? colDef.headers.map((v, i) => {
+                                    _colDef ? _colDef.headers ? _colDef.headers.map((v, i) => {
                                         return <th key={i} className="px-6 py-2 main-color text-left">
                                             {v.title}
                                         </th>
@@ -37,33 +31,28 @@ const SimpleTable = props => {
                                 </tr>
                             </thead>
                             <tbody className="bg-white">
-                           {
-                               (_data && _data.status != undefined ?
-                                    (_data.status == 1 ?
-                                        _data.data.map((r, i) => {
-                                            return (<tr 
+                            {
+                               (_tableData && _tableData.status != undefined ?
+                                    (_tableData.status == 1 ?
+                                        _tableData.data.map((r, i) => {
+                                            return <tr 
                                                         key={r.id} 
                                                         id={"tr" + r.id} 
-                                                        className="whitespace-nowrap"
-                                                    >{
+                                                        className="whitespace-nowrap">
+                                                    {
                                                         r.data.map((c, j) => {
                                                             return <td key={"col-" + j} className="px-6 py-4 main-color">{c.value}</td>
                                                         })
-                                                    }</tr>)
-                                        })
-                                    : _data.status == 2 ? // Waiting status
-                                        
-                                            <tr>
-                                                <td colSpan="8" style={{textAlign:'center'}}>
-                                                    <OcxSpinner size='16' />
-                                                </td>
-                                            </tr>
-                                        
-                                        : null
-                                    )
-                                : null
-                                )
-                                
+                                                    }
+                                                    </tr>
+                                            }
+                                        )
+                                        : _tableData.status == 2 ? // Waiting status
+                                            <tr><td colSpan="8" style={{textAlign:'center'}}>
+                                                <OcxSpinner size='16' />
+                                            </td></tr>                                    
+                                    : null)
+                                : null)
                             }
                             </tbody>
                         </table>
