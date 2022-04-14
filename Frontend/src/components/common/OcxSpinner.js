cd44117514ed953a3e923defabb54140052100f9
Victory Nicholas <vn649271@example.com>
2022-04-14 20:43:01 +1000

    - Introduced OcxSpinner component to SimpleTable one

---
+++ Frontend/src/components/common/OcxSpinner.js
@@ -0,0 +1,11 @@
+
+const OcxSpinner = props => {
+	return (	
+	    <div className="flex justify-center mt-10 mb-10">
+	        <div style={{'borderTopColor':'transparent'}}
+	            className="w-16 h-16 border-4 border-gray-300 border-solid rounded-full animate-spin"></div>
+	    </div>
+	);
+}
+
+export default OcxSpinner;
